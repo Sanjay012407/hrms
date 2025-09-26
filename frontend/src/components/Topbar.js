0e5cb6728c5eb346f2956690d69fc064bd3c4fa1
Kaveen-1222 <12kaveen@gmail.com>
2025-09-26 14:59:54 +0530

    Mapped again

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -30,11 +30,11 @@ export default function Topbar({ toggleSidebar }) {
       </button>
 
       {/* Search Box */}
-      <input
+      {/*<input
         type="text"
         placeholder="Search accounts..."
         className="border rounded px-3 py-1 w-1/3"
-      />
+      />*/}
 
       <div className="ml-auto flex items-center gap-4 relative">
         <span className="text-sm font-semibold">Account: Vitrux Ltd</span>
@@ -64,7 +64,7 @@ export default function Topbar({ toggleSidebar }) {
               <button
                 onClick={() => {
                   setOpen(false);
-                  navigate("/dashboard/createcertificate"); 
+                  navigate("/dashboard/createcretificate"); 
                 }}
                 className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
               >
