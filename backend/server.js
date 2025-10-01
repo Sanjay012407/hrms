f855437fbfdd7a19ef1a26474e6f7016f4c1c6b6
Kaveen-1222 <12kaveen@gmail.com>
2025-10-01 16:57:53 +0530

    Revert "Server"
    This reverts commit 207b9ac81a89e1decdd01536f83b47407044f12f.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const cron = require('node-cron');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
-const rateLimit = require('express-rate-limit');
 // Load environment configuration
 const envConfig = require('./config/environment');
 const config = envConfig.getConfig();
@@ -93,13 +92,6 @@ const validateCertificateInput = (req, res, next) => {
   
   next();
 };
-
-// Rate limiting for auth endpoints
-const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 requests per window
-  message: 'Too many login attempts, please try again later'
-});
 
 // MongoDB connection
 mongoose.connect(MONGODB_URI);
