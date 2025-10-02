a72b40d71ab603bb59681d0aa8a81c88cd4c9ab9
Naveen MV <mvnaveen18@gmail.com>
2025-10-02 20:14:59 +0530

    Bugfix

--- frontend/src/pages/CreateCertificate.js
+++ frontend/src/pages/CreateCertificate.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useCertificates } from "../context/CertificateContext";
 import { useProfiles } from "../context/ProfileContext";
 import { getCertificatesForMultipleJobRoles, getAllJobRoles, allCertificates } from "../data/certificateJobRoleMapping";
